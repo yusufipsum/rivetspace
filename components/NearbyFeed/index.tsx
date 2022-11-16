0efac526ecd2ae33e6c4616ab2703d8119267c75
Yusuf FINDIK <yusuf.findik@macellan.net>
2022-11-16 03:24:44 +0300

    Add top nearby users count info

--- components/NearbyFeed/index.tsx
+++ components/NearbyFeed/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { View, FlatList } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
+
+import styles from "./styles.ts";
 
 import profiles from "../../data/profiles";
 import NearbyProfile from "../NearbyProfile";
+import ProfilePicture from "../ProfilePicture";
 
 const NearbyFeed = () => (
-  <View style={{ width: "100%", flex: 1 }}>
+  <View style={styles.container}>
+    <View style={styles.topContainer}>
+      <FlatList
+        contentContainerStyle={styles.topFlatList}
+        horizontal
+        scrollEnabled={false}
+        data={profiles.slice(0, 5)}
+        renderItem={({ item }) => (
+          <ProfilePicture
+            marginRight={-10}
+            marginLeft={-10}
+            image={item.user.image}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
+      <Text style={styles.text}>
+        Ortamında {profiles.length} aktif kullanıcı bulunuyor
+      </Text>
+    </View>
     <FlatList
       data={profiles}
       renderItem={({ item }) => <NearbyProfile profiles={item} />}
---
+++ components/NearbyFeed/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flex: 1,
+  },
+  topContainer: {
+    width: "90%",
+    height: 90,
+    backgroundColor: "#C6EEFF",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 20,
+  },
+  topFlatList: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  text: {
+    paddingBottom: 7,
+  },
+});
+
+export default styles;
