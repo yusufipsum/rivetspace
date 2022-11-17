fae9537b13cd1a9a87f08c7e550f9c1d5cc845a3
Yusuf FINDIK <yusuf.findik@macellan.net>
2022-11-18 00:18:52 +0300

    Edit page

--- screens/NearbyScreen/index.tsx
+++ screens/NearbyScreen/index.tsx
@@ -1,17 +1,15 @@
 import * as React from "react";
+import { SafeAreaView } from "react-native";
 
 import styles from "./styles.ts";
 
-import { SafeAreaView } from "react-native";
-
 import { Text } from "../../components/Themed";
-
 import { NearbyFeed } from "../../components";
 
 export default function NearbyScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <NearbyFeed />
+      <NearbyFeed />  
     </SafeAreaView>
   );
 }
