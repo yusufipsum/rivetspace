0b30872412f3a9dcd8c6873fc700e84d69d07934
Yusuf FINDIK <yusuf.findik@macellan.net>
2022-11-19 01:53:24 +0300

    Edit nearby feed component

--- components/NearbyFeed/index.tsx
+++ components/NearbyFeed/index.tsx
@@ -5,32 +5,12 @@ import styles from "./styles.ts";
 
 import profiles from "../../data/profiles";
 import NearbyProfile from "../NearbyProfile";
+import NearbyTopInfo from "../NearbyTopInfo";
 import ProfilePicture from "../ProfilePicture";
 
 const NearbyFeed = () => (
   <View style={styles.container}>
-    <View style={styles.topContainer}>
-      <FlatList
-        contentContainerStyle={styles.topFlatList}
-        horizontal
-        scrollEnabled={false}
-        data={profiles.slice(0, 5)}
-        renderItem={({ item }) => (
-          <ProfilePicture
-            marginRight={-10}
-            marginLeft={-10}
-            image={item.user.image}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-      />
-      <Text style={styles.text}>
-        Ortamında <Text style={{ fontWeight: "bold" }}>{profiles.length}</Text>{" "}
-        aktif kullanıcı bulunuyor
-      </Text>
-    </View>
+    <NearbyTopInfo />
     <FlatList
       data={profiles}
       renderItem={({ item }) => <NearbyProfile profiles={item} />}
