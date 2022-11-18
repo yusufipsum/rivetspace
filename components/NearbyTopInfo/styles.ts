a69cf7b1088a1faa582049537eb69d2566cf4543
Yusuf FINDIK <yusuf.findik@macellan.net>
2022-11-19 01:52:41 +0300

    Separate nearby top info

---
+++ components/NearbyTopInfo/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  topContainer: {
+    width: "90%",
+    height: 90,
+    backgroundColor: "#C6EEFF",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 20,
+  },
+
+  topFlatList: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  text: {
+    paddingBottom: 7,
+    fontSize: 16,
+  },
+});
+
+export default styles;
