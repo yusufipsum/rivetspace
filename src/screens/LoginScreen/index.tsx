ae06afe7214b1015e6a9369f7265e09c2186300e
Yusuf FINDIK <yusuf.findik@macellan.net>
2022-12-11 23:57:57 +0300

    Create login screen

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -5,25 +5,98 @@ import {
   TouchableOpacity,
   StatusBar,
   SafeAreaView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  TextInput,
+  ScrollView,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import styles from "./styles";
-
-import { AntDesign } from "@expo/vector-icons";
+import { Logo } from "../../assets/svg";
 
 export default function LoginScreen() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Login Screen</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+  const navigation = useNavigation();
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
-    </SafeAreaView>
+  const Login = () => {
+    navigation.navigate("Home");
+  };
+
+  const Sign = () => {
+    navigation.navigate("SignIn");
+  };
+
+  const DismissKeyboard = ({ children }) => (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        {children}
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+  return (
+    <DismissKeyboard>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.sectionCenter}>
+          <View style={styles.logo}>
+            <Logo size={140} />
+            <Text style={styles.title}>rivetspace</Text>
+          </View>
+          <View style={styles.form}>
+            <View style={styles.inputs}>
+              <View style={styles.input}>
+                <TextInput
+                  type="text"
+                  fontSize={15}
+                  width={"100%"}
+                  placeholder="Kullanıcı adı"
+                  placeholderFontWeight="bold"
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  secureTextEntry={true}
+                  fontSize={15}
+                  width={"100%"}
+                  placeholder="Şifre"
+                  placeholderFontWeight="bold"
+                />
+              </View>
+            </View>
+            <View style={styles.footer}>
+              <View style={styles.forgotPassword}>
+                <TouchableOpacity raised activeOpacity={0.8}>
+                  <Text
+                    style={{
+                      color: "grey",
+                    }}
+                  >
+                    Şifremi Unuttum
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity
+                raised
+                activeOpacity={0.8}
+                style={styles.button}
+                onPress={Login}
+              >
+                <Text style={styles.buttonText}>Giriş Yap</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.sign}>
+            <Text style={styles.text}>Henüz bir hesabın yok mu? </Text>
+            <TouchableOpacity raised activeOpacity={0.6} onPress={Sign}>
+              <Text style={{ fontSize: "14", fontWeight: "bold" }}>Kaydol</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+      </SafeAreaView>
+    </DismissKeyboard>
   );
 }
--- src/screens/LoginScreen/styles.ts
+++ src/screens/LoginScreen/styles.ts
@@ -4,18 +4,81 @@ import Colors from "../../constants/Colors";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  logo: {
+    alignItems: "center",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
+  title: {
+    fontFamily: "Baskerville-Bold",
+    fontWeight: "bold",
+    fontSize: 34,
+    color: "black",
+  },
+  sectionCenter: {
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "100%",
+    height: "auto",
+  },
+  form: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 300,
+  },
+  input: {
+    flexDirection: "row",
+    alignItems: "center",
     width: "80%",
+    height: 35,
+    borderBottomWidth: 1,
+    borderColor: "#f7f7f7",
+  },
+  inputs: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    height: 90,
+  },
+  forgotPassword: {
+    alignItems: "flex-end",
+    width: "80%",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: 60,
+    backgroundColor: "black",
+    borderRadius: 30,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "white",
+  },
+  footer: {
+    marginTop: 10,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 110,
+    width: "100%",
+  },
+  text: {
+    fontSize: 14,
+    color: "grey",
+  },
+  sign: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
 
