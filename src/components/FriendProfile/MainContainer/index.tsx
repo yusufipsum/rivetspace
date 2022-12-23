c48cc9f17b972cd9d638e1461b2015a7f904b307
Yusuf Fındık <yusuf.findik@macellan.net>
2022-12-23 15:48:37 +0300

    Delete some imports

--- src/components/FriendProfile/MainContainer/index.tsx
+++ src/components/FriendProfile/MainContainer/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
-import { Ionicons, Octicons } from "@expo/vector-icons";
 /*import moment from 'moment';*/
 
 import { ProfileType } from "../../../types";
