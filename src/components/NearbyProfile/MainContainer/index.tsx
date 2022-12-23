bed4868b4f306c99f9426a46f99e7221c3c2c111
Yusuf Fındık <yusuf.findik@macellan.net>
2022-12-23 15:50:28 +0300

    Delete some imports

--- src/components/NearbyProfile/MainContainer/index.tsx
+++ src/components/NearbyProfile/MainContainer/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 import { Ionicons, Octicons } from "@expo/vector-icons";
