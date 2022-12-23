2e53acfbd468e4d2b4263697dfed223ff1bd8a95
Yusuf Fındık <yusuf.findik@macellan.net>
2022-12-23 15:57:11 +0300

    Delete some imports

--- src/screens/FriendsScreen/index.tsx
+++ src/screens/FriendsScreen/index.tsx
@@ -1,12 +1,6 @@
 import * as React from "react";
-import {
-  StyleSheet,
-  TouchableOpacity,
-  SafeAreaView,
-  TextInput,
-} from "react-native";
+import { TouchableOpacity, SafeAreaView } from "react-native";
 
-import { SearchBar } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 
 import styles from "./styles.ts";
