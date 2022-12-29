7a2ecb25957efc53fc09a3036b03abb37c0d299c
Yusuf Fındık <yusuf.findik@macellan.net>
2022-12-29 16:48:46 +0300

    Edit NotificationFeed line

--- src/screens/NotificationScreen/index.tsx
+++ src/screens/NotificationScreen/index.tsx
@@ -43,7 +43,7 @@ export default function NotificationsScreen() {
           lightColor="#eee"
           darkColor="rgba(255,255,255,0.1)"
         />
-        <NotificationFeed />
+        {/* <NotificationFeed /> */}
         {/* Use a light status bar on iOS to account for the black space above the modal */}
         <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
       </View>
