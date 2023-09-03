b03739d260149b65e6c09f84652a44cad4d77856
y <lordsploit@Yusufhs-MBP.Dlink>
2023-09-03 15:49:28 +0300

    Check and update uuid attribute of user

--- App.tsx
+++ App.tsx
@@ -12,13 +12,23 @@ import awsconfig from "./src/aws-exports";
 import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
+
 import DismissKeyboard from "./src/components/DismissKeyboard";
+
 import { createUser } from './src/graphql/mutations';
 import { getUser } from "./src/graphql/queries";
+
+import DeviceInfo from "react-native-device-info";
+//import BLEAdvertiser from "react-native-ble-advertiser";
 
 Amplify.configure(awsconfig);
 
 function App() {
+  // const APPLE_ID = 0x4c;
+  // const MANUF_DATA = [1, 0];
+
+  // BLEAdvertiser.setCompanyId(APPLE_ID);
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -34,8 +44,31 @@ function App() {
     console.log("sadfsdafsad", user);
   }
 
-  useEffect(() => {
+  const [mac, setMac] = useState("");
+  const [customUUID, setCustomUUID] = useState("");
 
+  const getMac = async () => {
+    DeviceInfo.getMacAddress()
+    .then(macAddress => {
+         console.log("That's my MAC Address:", macAddress)
+         setMac(macAddress);
+     })
+     .catch(error => console.log("error", error))
+  }
+
+  // const startAdv = () => { 
+  //   BLEAdvertiser.broadcast(customUUID, MANUF_DATA, {
+  //   //advertiseMode: 2, (BLEAdvertiser.ADVERTISE_MODE_BALANCED,)
+  //   //txPowerLevel: 4, (BLEAdvertiser.ADVERTISE_TX_POWER_MEDIUM,)
+  //   connectable: false,
+  //   includeDeviceName: false,
+  //   includeTxPowerLevel: false,
+  // })
+  //   .then((sucess) => console.log(customUUID, 'Broadcast Successful', sucess))
+  //   .catch((error) => console.log(customUUID, 'Broadcast Error', error));
+  // }
+
+  useEffect(() => {
     Hub.listen("auth", (e) => {
       if (e.payload.event == "signIn") {
         setCurrentUser(e.payload.data);
@@ -47,11 +80,18 @@ function App() {
     const updateUser = async () => {
       //get current authenticated user
       const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
-
+      setCustomUUID(userInfo.attributes['custom:UUID']);
       if (userInfo) {
         //check if user already exist in database
         const userData = await API.graphql(graphqlOperation(getUser, { id: userInfo.attributes.sub }))
-        console.log("aa", userInfo);
+        //console.log("aa", userInfo);
+        if(customUUID !== mac){
+          const user = await Auth.currentAuthenticatedUser();
+          const response = await Auth.updateUserAttributes(user, {
+            'custom:UUID': `${mac}`,
+          });
+          console.log("update user", response);
+        }
         if (!userData.data.getUser){
           const user = {
             id: userInfo.attributes.sub,
@@ -67,8 +107,10 @@ function App() {
       }
       //if doesn't, create the user in the database
     }
+    getMac();
     updateUser();
-  }, [])
+    //startAdv();
+  }, [customUUID]);
 
   if (!isLoadingComplete) {
     return null;
