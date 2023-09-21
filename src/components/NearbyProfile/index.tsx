8b3282bcec0522a7461d6a20013cde64180251d8
y <lordsploit@Yusufhs-MBP.Dlink>
2023-09-21 16:21:42 +0300

    Add user background color

--- src/components/NearbyProfile/index.tsx
+++ src/components/NearbyProfile/index.tsx
@@ -7,6 +7,7 @@ import LeftContainer from "./LeftContainer";
 import MainContainer from "./MainContainer";
 
 import { ProfileType } from "../../types";
+import Background from "../Backgrounds";
 
 export type ProfileProps = {
   profiles: ProfileType;
@@ -15,6 +16,7 @@ export type ProfileProps = {
 const Profile = ({ profiles }: ProfileProps) => (
   <View style={styles.cards}>
     <View style={styles.container}>
+    <Background color={profiles.user.color} />
       <LeftContainer user={profiles.user} />
       <MainContainer profiles={profiles} />
     </View>
