487543552326f9c60dcbea1b8a63bcdb087867b3
y <lordsploit@Yusufhs-MBP.Dlink>
2023-11-08 14:38:58 +0300

    Fix necessary additions

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const blacklist = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig } = require("metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }, transformer,
+  } = await getDefaultConfig();
+  return {  
+    resolver: {
+      blacklistRE: blacklist([/#current-cloud-backend\/.*/]),
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+    },
+    
+  };
+})();
