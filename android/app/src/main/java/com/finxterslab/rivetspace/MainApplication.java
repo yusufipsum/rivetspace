1f5d68a14924025e1698dbcef1dd980293726948
y <lordsploit@Yusufhs-MBP.Dlink>
2023-11-08 14:28:50 +0300

    Android things

--- android/app/src/main/java/com/finxterslab/rivetspace/MainApplication.java
+++ android/app/src/main/java/com/finxterslab/rivetspace/MainApplication.java
@@ -20,6 +20,8 @@ import expo.modules.ReactNativeHostWrapper;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.airbnb.android.react.lottie.LottiePackage; 
+
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
     this,
@@ -33,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
+      packages.add(new LottiePackage());
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
